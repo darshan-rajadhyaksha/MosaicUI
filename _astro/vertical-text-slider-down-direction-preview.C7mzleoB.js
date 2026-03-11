7f5ca8b4004a6bb3da05668d566c567f80f57230
darshan-rajadhyaksha <darshan-rajadhyaksha@users.noreply.github.com>
2026-03-11 05:01:19 +0000

    deploy: f5c5ada924a5f6c89a67ceaa1fdf4f61b654e990

---
+++ _astro/vertical-text-slider-down-direction-preview.C7mzleoB.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime.D_zvdyIk.js";import{V as r}from"./vertical-text-slider.eKKD96MD.js";import"./index.DiEladB3.js";import"./_component_.abe5225a.CWC7eG7m.js";const l=()=>e.jsx("h2",{children:e.jsx(r,{texts:["Build faster","Ship smarter","Scale confidently","Delight users"],direction:"down"})});export{l as default};
