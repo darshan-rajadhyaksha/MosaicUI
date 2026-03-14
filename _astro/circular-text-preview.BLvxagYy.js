c0491e3541ae03cf80e10901fffb1e0691617498
darshan-rajadhyaksha <darshan-rajadhyaksha@users.noreply.github.com>
2026-03-14 05:29:36 +0000

    deploy: 122fe462619e5c03698137db71dcae63fbb94fe4

--- _astro/circular-text-preview.BLvxagYy.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./jsx-runtime.D_zvdyIk.js";import{C as t}from"./circular-text.Bdl_WKqr.js";import"./index.DiEladB3.js";import"./_component_.abe5225a.BCLqLiy3.js";const s=()=>r.jsx(t,{text:"CODE • DESIGN • SHIP •",radius:80,rotate:!0});export{s as default};
---
+++ _astro/vertical-text-slider-down-direction-preview.C8kUcr_X.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime.D_zvdyIk.js";import{V as r}from"./vertical-text-slider.CtSsF4f1.js";import"./index.DiEladB3.js";import"./_component_.abe5225a.BUHcIapd.js";const l=()=>e.jsx("h2",{children:e.jsx(r,{texts:["Build faster","Ship smarter","Scale confidently","Delight users"],direction:"down"})});export{l as default};
