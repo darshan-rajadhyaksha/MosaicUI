9ed21504b9ed248ee9cd90c187f655e012209e4c
darshan-rajadhyaksha <darshan-rajadhyaksha@users.noreply.github.com>
2026-03-14 14:07:53 +0000

    deploy: 78f5afe94664028fc61340f44ef3b5b725ede8ee

--- _astro/colorized-text-style-letter-preview.CL-1su3K.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./jsx-runtime.D_zvdyIk.js";import{C as r}from"./colorized-text.Co6c3uA2.js";import"./index.DiEladB3.js";import"./_component_.abe5225a.BUHcIapd.js";const F=()=>t.jsx("h1",{style:{fontSize:"5rem"},children:t.jsx(r,{variant:"standard",colors:["#FF0000","#FFA500","#FFFF00","#008000","#0000FF","#4B0082","#EE82EE"],text:"Rainbow"})});export{F as default};
