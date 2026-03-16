a812fb941dc921ecebadc8e841da73ca45cc3c33
darshan-rajadhyaksha <darshan-rajadhyaksha@users.noreply.github.com>
2026-03-16 13:16:49 +0000

    deploy: 7d7cd5d79f26c8280069618e0ae9fecb96697619

--- _astro/circular-text-anti-clockwise-direction-preview.BVJOxwJk.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./jsx-runtime.D_zvdyIk.js";import{C as r}from"./circular-text.BYrGCYsw.js";import"./index.DiEladB3.js";import"./_component_.abe5225a.soq4HQcl.js";const a=()=>t.jsx(r,{text:"CODE • DESIGN • SHIP •",radius:85,rotate:!0,direction:"anti-clockwise"});export{a as default};
