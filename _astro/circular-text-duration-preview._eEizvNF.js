f30ea168ab1127432e291ba71c2962b28fb509bd
darshan-rajadhyaksha <darshan-rajadhyaksha@users.noreply.github.com>
2026-03-17 12:06:29 +0000

    deploy: 85589e34ae0301eb74a4b869ac289de294be5990

---
+++ _astro/circular-text-duration-preview._eEizvNF.js
@@ -0,0 +1,1 @@
+import{j as r}from"./jsx-runtime.D_zvdyIk.js";import{C as t}from"./circular-text.6Fvvf1i7.js";import"./index.DiEladB3.js";import"./_component_.abe5225a.DJupA9oY.js";const u=()=>r.jsx(t,{text:"CODE • DESIGN • SHIP •",radius:85,rotate:!0,duration:5e3});export{u as default};
