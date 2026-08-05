af791743123e07dd8680d2b15adb52635c541833
darshan-rajadhyaksha <d0rajadhyaksha@gmail.com>
2026-08-05 19:20:42 +0530

    Component: Grid Pattern Background

---
+++ src/docs/backgrounds/grid-pattern-background/preview.tsx
@@ -0,0 +1,36 @@
+import GridPatternBackground from "@/registry/backgrounds/grid-pattern-background/grid-pattern-background";
+
+const GridPatternBackgroundPreview = () => {
+  return (
+    <div className="relative w-full h-full grid place-items-center">
+      <GridPatternBackground 
+        direction="top-left"
+        size={25}
+      />
+      <HeroSection />
+    </div>
+  );
+};
+
+const HeroSection = () => (
+  <section className="relative z-10 flex items-center justify-center p-8">
+    <div className="mx-auto max-w-md text-center">
+      <div className="mb-5 lg:mb-6 inline-flex items-center leading-0 rounded-full border border-gray-300 dark:border-white/15 px-4 py-4 text-sm font-medium text-gray-800 dark:text-white backdrop-blur-md">
+        ⚛️ Background Component
+      </div>
+      <h1 className="text-4xl font-bold tracking-tight text-gray-800 dark:text-white lg:text-5xl text-balance">
+        Cartesian Coordinates Never Miss Meetings
+      </h1>
+      <p className="mx-auto mt-6 max-w-md text-md lg:text-lg leading-6 text-gray-500 dark:text-white/70 text-balance">
+        Every intersection has a purpose. Every pixel has accountability. Revolutionary, really.
+      </p>
+      <div className="mt-6 lg:mt-8">
+        <button className="rounded-full border border-gray-300 dark:border-white/20 border-gray-50 bg-white/10 px-8 py-3 text-sm font-semibold text-gray-800 dark:text-white backdrop-blur-md transition-all duration-300 hover:bg-gray-100/50 dark:hover:bg-white/20">
+          Get Component
+        </button>
+      </div>
+    </div>
+  </section>
+);
+
+export default GridPatternBackgroundPreview;
